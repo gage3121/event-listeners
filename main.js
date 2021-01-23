476a01210b1cb0f0d4a2613909b76a575c89608c
gage3121 <gagenhall@gmail.com>
2021-01-23 11:08:45 -0600

    Practiced callbacks

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-console.log('connected')
+//console.log('connected')
+
+//const button = document.getElementById('main-btn')
+
+//console.log(button)
+
+//const button2 = document.querySelector('button')
+
+//console.log(button2)
+
+function sayHi(user) {
+    return `Hi ${user}!`;
+}
+
+function sayBye(user) {
+    return `Bye ${user}!`;
+}
+
+function createGreeting(user, call) {
+    return call(user);
+}
+
+console.log(createGreeting('Buster', sayHi)); 
+console.log(createGreeting('Buster', sayBye));
