1b77d167653f0af3b774ba81df92ea9115dc02d1
gage3121 <gagenhall@gmail.com>
2021-01-23 10:57:16 -0600

    tested selectors

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log('connected')
+//console.log('connected')
+
+const button = document.getElementById('main-btn')
+
+console.log(button)
+
+const button2 = document.querySelector('button')
+
+console.log(button2)
